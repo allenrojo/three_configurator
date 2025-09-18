4f30e2030e69a3dbd8437e208d52b0d3577485c0
Allen <allenrojo31@gmail.com>
2025-09-18 09:50:57 +0800

    initial setup

---
+++ src/js/scripts.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(0,2,5);
+orbit.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  orbit.update(); 
+  renderer.render(scene, camera);
+}
+
+animate();
